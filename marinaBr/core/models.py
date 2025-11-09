1157aee40b7ab82805a7e2dc4f3fc90bfbf63993
Maks-am-I <newstepms@yandex.ru>
2025-11-09 16:50:30 +0700

    feat: add model Product and make migrations

--- marinaBr/core/models.py
+++ marinaBr/core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    description = models.TextField(blank=True)
+    is_published = models.BooleanField(default=True)
+    category = models.CharField(max_length=100)
+
